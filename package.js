3025abd16540ba694de53c3dd7c722427c9d9a72
David Robbins <robbins.david@gmail.com>
2014-10-20 07:20:52 -0500

    Published 1.7.1_2 version. Removed export comment

--- package.js
+++ package.js
@@ -1,14 +1,13 @@
 Package.describe({
   name: "robbinsd:dc",
   summary: "Multi-Dimensional charting built to work natively with crossfilter rendered with d3.js",
-  version: "1.7.0",
+  version: "1.7.1_2",
   git: "https://github.com/drobbins/meteor-dc.git"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom("METEOR@0.9.4");
   api.use(["sergeyt:d3@3.4.1","pfafman:crossfilter@1.3.9"], "client");
-  //if (api.export) api.export("dc", "client");
   api.addFiles("dc.js", "client");
 });
 
