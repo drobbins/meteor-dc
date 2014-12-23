3e49cef56386a05d733c4c4ed0b0e511376abbda
David Robbins <robbins.david@gmail.com>
2014-12-23 08:31:10 -0600

    Added CSS. See #1

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: "robbinsd:dc",
   summary: "Multi-Dimensional charting built to work natively with crossfilter rendered with d3.js",
-  version: "1.7.1_2",
+  version: "1.7.1_3",
   git: "https://github.com/drobbins/meteor-dc.git"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom("METEOR@0.9.4");
   api.use(["sergeyt:d3@3.4.1","pfafman:crossfilter@1.3.9"], "client");
-  api.addFiles("dc.js", "client");
+  api.addFiles(["dc.js", "dc.css"], "client");
 });
 
 Package.onTest(function(api) {
