940003988494f61bed14754e1db48fffb035dee6
David Robbins <robbins.david@gmail.com>
2014-12-23 09:11:05 -0600

    Added back dependency version tests

--- dc-tests.js
+++ dc-tests.js
@@ -3,3 +3,9 @@
 Tinytest.add("Includes dc.js version 1.7.3", function (test) {
   test.equal(dc.version, "1.7.3");
 });
+Tinytest.add("Include d3.js version 3.4.13", function (test) {
+    test.equal(Package["d3js:d3"].d3.version, "3.4.13");
+});
+Tinytest.add("Include crossfilter version 1.3.9", function (test) {
+    test.equal(crossfilter.version, "1.3.9");
+});
